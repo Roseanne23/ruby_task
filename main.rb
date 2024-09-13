668be5fd60429cf59782398058e21b7449afdce7
Roseanne23 <>
2024-09-13 10:49:28 +0800

    create an array of people

--- main.rb
+++ main.rb
@@ -1,19 +1,26 @@
 #```ruby
 #  ```
-persons = {
-  person1: { national_id: "001", name: "Joe", age: "20" },
-  person2: { national_id: "002", name: "Mark", age: "15" },
-  person3: { national_id: "003", name: "Kris", age: "35" },
-  person4: { national_id: "004", name: "Melody", age: "28" },
-  person5: { national_id: "005", name: "Stacy", age: "13" },
-  person6: { national_id: "006", name: "Lovi", age: "32" },
-  person7: { national_id: "007", name: "Carrie", age: "42" },
-  person8: { national_id: "008", name: "Randy", age: "25" },
-  person9: { national_id: "009", name: "Rika", age: "21" },
-  person10: { national_id: "010", name: "Bill", age: "50"}
-}
-  p persons
-
-
-
+persons = [
+  person1: { national_id: "001", name: "Joe", age: "20" } ,
+  person2: { national_id: "002", name: "Mark", age: "15" } ,
+  person3: { national_id: "003", name: "Kris", age: "35" } ,
+  person4: { national_id: "004", name: "Melody", age: "28" } ,
+  person5: { national_id: "005", name: "Stacy", age: "13" } ,
+  person6: { national_id: "006", name: "Lovi", age: "32" } ,
+  person7: { national_id: "007", name: "Carrie", age: "42" } ,
+  person8: { national_id: "008", name: "Randy", age: "25" } ,
+  person9: { national_id: "009", name: "Rika", age: "21" } ,
+  person10: { national_id: "010", name: "Bill", age: "50"} ,
+  person11: { national_id: "011", name: "Kate", age: "65"} ,
+  person12: { national_id: "012", name: "Rome", age: "70"} ,
+  person13: { national_id: "013", name: "Vanessa", age: "48"} ,
+  person14: { national_id: "014", name: "Billie", age: "45"} ,
+  person15: { national_id: "015", name: "Mori", age: "51"} ,
+  person16: { national_id: "016", name: "Pat", age: "29"} ,
+  person17: { national_id: "017", name: "Paul", age: "16"} ,
+  person18: { national_id: "018", name: "Yor", age: "52"} ,
+  person19: { national_id: "019", name: "Anna", age: "17"} ,
+  person20: { national_id: "020", name: "Rick", age: "78 "}
+]
+p persons
 
