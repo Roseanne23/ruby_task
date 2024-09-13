df35a9c3c8f0e26e717764337561f3b6d9aeff95
Roseanne23 <>
2024-09-13 12:47:47 +0800

    add a new user

--- main.rb
+++ main.rb
@@ -1,26 +1,43 @@
 #```ruby
 #  ```
 persons = [
-  person1: { national_id: "001", name: "Joe", age: "20" } ,
-  person2: { national_id: "002", name: "Mark", age: "15" } ,
-  person3: { national_id: "003", name: "Kris", age: "35" } ,
-  person4: { national_id: "004", name: "Melody", age: "28" } ,
-  person5: { national_id: "005", name: "Stacy", age: "13" } ,
-  person6: { national_id: "006", name: "Lovi", age: "32" } ,
-  person7: { national_id: "007", name: "Carrie", age: "42" } ,
-  person8: { national_id: "008", name: "Randy", age: "25" } ,
-  person9: { national_id: "009", name: "Rika", age: "21" } ,
-  person10: { national_id: "010", name: "Bill", age: "50"} ,
-  person11: { national_id: "011", name: "Kate", age: "65"} ,
-  person12: { national_id: "012", name: "Rome", age: "70"} ,
-  person13: { national_id: "013", name: "Vanessa", age: "48"} ,
-  person14: { national_id: "014", name: "Billie", age: "45"} ,
-  person15: { national_id: "015", name: "Mori", age: "51"} ,
-  person16: { national_id: "016", name: "Pat", age: "29"} ,
-  person17: { national_id: "017", name: "Paul", age: "16"} ,
-  person18: { national_id: "018", name: "Yor", age: "52"} ,
-  person19: { national_id: "019", name: "Anna", age: "17"} ,
-  person20: { national_id: "020", name: "Rick", age: "78 "}
+  { national_id: 1, name: "Joe", age: 20},
+  { national_id: 2, name: "Mark", age: 15},
+  { national_id: 3, name: "Kris", age: 35},
+  { national_id: 4, name: "Melody", age: 28},
+  { national_id: 5, name: "Stacy", age: 13},
+  { national_id: 6, name: "Lovi", age: 32},
+  { national_id: 7, name: "Carrie", age: 42},
+  { national_id: 8, name: "Randy", age: 25},
+  { national_id: 9, name: "Rika", age: 21},
+  { national_id: 10, name: "Bill", age: 50},
+  { national_id: 11, name: "Kate", age: 65},
+  { national_id: 12, name: "Rome", age: 70},
+  { national_id: 13, name: "Vanessa", age: 48},
+  { national_id: 14, name: "Billie", age: 45},
+  { national_id: 15, name: "Mori", age: 51},
+  { national_id: 16, name: "Pat", age: 29},
+  { national_id: 17, name: "Paul", age: 16},
+  { national_id: 18, name: "Yor", age: 52},
+  { national_id: 19, name: "Anna", age: 17},
+  { national_id: 20, name: "Rick", age: 78}
 ]
-p persons
+#puts persons
 
+puts "Please enter your national id"
+national_id = gets.chomp.to_i
+
+if persons.any? {|person| person[:national_id] == national_id}
+  puts "Failed to add: National ID already exists."
+else
+  puts "What is your name?"
+end
+name = gets.chomp
+puts "What is your age  ?"
+age = gets.chomp.to_i
+
+new_person = {national_id: national_id, name: name, age: age}
+persons.insert(0, new_person)
+
+puts "User added successfully!"
+puts persons
