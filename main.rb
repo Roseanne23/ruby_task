5d14fadc9b88492f9b39fc86fb65b0cdee833598
Roseanne23 <>
2024-09-13 10:33:15 +0800

    create an array of people

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
-```ruby
-        puts 'Hello World'
-   ```
+#```ruby
+#  ```
+persons = {
+  person1: { national_id: "001", name: "Joe", age: "20" },
+  person2: { national_id: "002", name: "Mark", age: "15" },
+  person3: { national_id: "003", name: "Kris", age: "35" },
+  person4: { national_id: "004", name: "Melody", age: "28" },
+  person5: { national_id: "005", name: "Stacy", age: "13" },
+  person6: { national_id: "006", name: "Lovi", age: "32" },
+  person7: { national_id: "007", name: "Carrie", age: "42" },
+  person8: { national_id: "008", name: "Randy", age: "25" },
+  person9: { national_id: "009", name: "Rika", age: "21" },
+  person10: { national_id: "010", name: "Bill", age: "50"}
+}
+  p persons
+
+
+
+
